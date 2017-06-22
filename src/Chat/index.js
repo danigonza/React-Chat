34afda759efd636b51d6b013f102f7fd4b590340
Daniel Gonzalez Lareo <danigonza86@gmail.com>
2017-06-22 11:21:56 +0200

    Initial component Chat

---
+++ src/Chat/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Chat extends Component {
+    render() {
+        return (
+            <div className="Chat">
+                This is a chat
+            </div>
+        );
+    }
+}
+
+export default Chat;
