c0793dae11d9f9b068b51c9af3bff72f57e8577a
Daniel Gonzalez Lareo <danigonza86@gmail.com>
2017-06-23 12:56:16 +0200

    Clean test

--- src/Chat/TextArea/index.test.js
+++ src/Chat/TextArea/index.test.js
@@ -8,21 +8,3 @@ it('renders correctly', () => {
     ).toJSON();
     expect(tree).toMatchSnapshot();
 });
-
-it('value changes when input is modified', () => {
-    // const component = renderer.create(<TextArea/>);
-    // let tree = component.toJSON();
-    // expect(tree).toMatchSnapshot();
-    //
-    // // manually trigger the callback
-    // tree.props.onMouseEnter();
-    // // re-rendering
-    // tree = component.toJSON();
-    // expect(tree).toMatchSnapshot();
-    //
-    // // manually trigger the callback
-    // tree.props.onMouseLeave();
-    // // re-rendering
-    // tree = component.toJSON();
-    // expect(tree).toMatchSnapshot();
-});
